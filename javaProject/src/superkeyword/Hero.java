856e66e0ec5c17f942dd917f0b12fc6e17aaf41d
BENFERJANI Khouloud <ferjanikhouloud@gmail.com>
2024-02-15 09:46:48 +0100

    comments

--- javaProject/src/superkeyword/Hero.java
+++ javaProject/src/superkeyword/Hero.java
@@ -1,16 +1,16 @@
 package superkeyword;
 
 public class Hero extends Person{
-
+ //sous class cad on prend les anciens parametres de person et ajouter des nouveaux parametres dans ce cas (+power)
 
 	String power;
 	
 	Hero(String name,int age,String power){	
-		super(name,age);
+		super(name,age); // rappeler des parametres pris 
 		this.power = power;
 	}
 	
-	public String toString() {
-		return super.toString()+this.power;
+   public String toString() { // to string lié à return 
+   return super.toString()+this.power; // si on met les 2 derniers lignes en comment l'affichage sera liée du return class person
 	}
 }
