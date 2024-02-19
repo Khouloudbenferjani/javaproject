db6119d39931194046d90e9d2e0123f646f12719
BENFERJANI Khouloud <ferjanikhouloud@gmail.com>
2024-02-19 16:13:44 +0100

    add new packages: polymorphism, dynamic polymorphism, handling, file reader, file writer

---
+++ javaProject/src/interfaces/Main.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Fish fish = new Fish();
+		
+		fish.hunt();
+		fish.flee();
+				
+	}
+}
---
+++ javaProject/src/polymorphism/Main.java
@@ -0,0 +1,24 @@
+package polymorphism;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// polymorphism = 	greek word for poly-"many", morph-"form"
+		//					The ability of an object to identify as more than one type
+		
+		Car car = new Car();
+		Bicycle bicycle = new Bicycle();
+		Boat boat = new Boat();
+		
+		
+		
+		// LES LIGNES SUIVANTES POUR AFFICHER LES RESULTATS
+		Vehicle[] racers = {car,bicycle,boat}; 
+		
+		for(Vehicle x : racers) {
+			x.go();
+		}
+		
+	}
+}
